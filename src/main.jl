89eec82d701579b1949ab25a77ddf6b78e4cceb9
Daniel Celis Garza <celisdanieljr@gmail.com>
2020-02-02 16:48:30 +0000

    Changed absolute paths for relative paths.

--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,7 @@
 using DDD
 
-inFilename = "C:/Users/Daniel Celis Garza/.julia/dev/DDD/inputs/simParams/sample"
-outFilename = "C:/Users/Daniel Celis Garza/.julia/dev/DDD/outputs/simParams/sample"
+inFilename = "./inputs/simParams/sample"
+outFilename = "./outputs/simParams/sample"
 dlnParams, matParams, intParams = loadParams(inFilename)
 saveParams(dlnParams, matParams, intParams, outFilename; delim = ',')
 dlnParams2, matParams2, intParams2 = loadParams(outFilename)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using DDD
 using Test
 
-inFilename = "C:/Users/Daniel Celis Garza/.julia/dev/DDD/inputs/simParams/sample"
-outFilename = "C:/Users/Daniel Celis Garza/.julia/dev/DDD/outputs/simParams/sample"
+inFilename = "./inputs/simParams/sample"
+outFilename = "./outputs/simParams/sample"
 dlnParams, matParams, intParams = loadParams(inFilename)
 saveParams(dlnParams, matParams, intParams, outFilename; delim = ',')
 dlnParams2, matParams2, intParams2 = loadParams(outFilename)
