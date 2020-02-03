f3dcc4256609a0c1ea01dc9b49ed87cd8922c779
Daniel Celis Garza <celisdanieljr@gmail.com>
2020-02-03 00:07:49 +0000

    Single test is failing on linux. Trying to fix it.

--- test/genSegTest.jl
+++ test/genSegTest.jl
@@ -12,14 +12,17 @@ slipSystem = convert.(Float64, Vector(df[slipSysInt, :]))
 edge = makeSegment!(dlnEdge(), slipSysInt, df)
 screw = makeSegment!(dlnScrew(), slipSysInt, df)
 
+edgeDscrew = dot(edge, screw)
+edgeDbVec = dot(edge, slipSystem[4:6])
+edgeTest = cross(slipSystem[1:3], slipSystem[4:6])
+edgeTest ./= norm(edgeTest)
+normEdge = norm(edge)
+normScrew = norm(screw)
+
 @testset "Generate single segments" begin
-    @test isapprox(dot(edge, screw), 0.0)
-    @test isapprox(dot(edge, slipSystem[4:6]), 0.0)
-    @test isapprox(
-        edge,
-        cross(slipSystem[1:3], slipSystem[4:6]) ./
-        norm(cross(slipSystem[1:3], slipSystem[4:6])),
-    )
-    @test isapprox(norm(edge), norm(screw))
-    @test isapprox(norm(edge), 1.0)
+    @test isapprox(edgeDscrew, 0.0)
+    @test isapprox(edgeDbVec, 0.0)
+    @test isapprox(edge, edgeTest)
+    @test isapprox(normEdge, normScrew)
+    @test isapprox(normEdge, 1.0)
 end
