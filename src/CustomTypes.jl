8f12c9fa095c90b7e7e7098894705b0ccfdc624e
Daniel Celis Garza <celisdanieljr@gmail.com>
2020-02-09 18:54:18 +0000

    Ensured normalisation of vector in arbitrary 3D rotation.

--- src/CustomTypes.jl
+++ src/CustomTypes.jl
@@ -32,7 +32,12 @@ end
 function extAngle(numSides::Integer)
     return π - intAngle(numSides)
 end
-
+"""
+```
+arbRot3D(abc::Vector{<:Real}, uvw::Vector{Real}, vec::Vector{<:Real}, θ::Real)
+```
+Rotate point (x,y,z) about the line with direction vector <u,v,w> that crosses the point (a,b,c) by the angle θ.
+"""
 function arbRot3D(
     abc::Vector{<:Real},
     uvw::Vector{Real},
@@ -43,25 +48,27 @@ function arbRot3D(
     local costheta = cos(θ)
     local onemcostheta = 1 - costheta
     local sintheta = sin(θ)
-    local dotxyzuvw = dot(xyz, uvw)
+    local xyzDOTuvw = dot(xyz, uvw)
+
+    isapprox(norm(uvw), 1.0) ? nothing : uvw ./= norm(uvw)
 
     return [
         (abc[1] * (uvw[2]^2 + uvw[3]^2) -
-         uvw[1] * (abc[2] * uvw[2] + abc[3] * uvw[3] - dotxyzuvw)) *
+         uvw[1] * (abc[2] * uvw[2] + abc[3] * uvw[3] - xyzDOTuvw)) *
         onemcostheta +
         xyz[1] * costheta +
         sintheta * (-abc[3] * uvw[2] + abc[2] * uvw[3] - uvw[3] * xyz[2] +
          uvw[2] * xyz[3])
 
         (abc[2] * (uvw[1]^2 + uvw[3]^2) -
-         uvw[2] * (abc[1] * uvw[1] + abc[3] * uvw[3] - dotxyzuvw)) *
+         uvw[2] * (abc[1] * uvw[1] + abc[3] * uvw[3] - xyzDOTuvw)) *
         onemcostheta +
         xyz[2] * costheta +
         sintheta *
         (abc[3] * uvw[1] - abc[1] * uvw[3] + uvw[3] * xyz[1] - uvw[1] * xyz[3])
 
         (abc[3] * (uvw[1]^2 + uvw[2]^2) -
-         uvw[3] * (abc[1] * uvw[1] + abc[2] * uvw[2] - dotxyzuvw)) *
+         uvw[3] * (abc[1] * uvw[1] + abc[2] * uvw[2] - xyzDOTuvw)) *
         onemcostheta +
         xyz[3] * costheta +
         sintheta * (-abc[2] * uvw[1] + abc[1] * uvw[2] - uvw[2] * xyz[1] +
