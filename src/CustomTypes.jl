4c47fe8b066a796c548d71aa668376334ececeea
Daniel Celis Garza <celisdanieljr@gmail.com>
2020-02-09 02:21:27 +0000

    Added rotational matrix about an arbitrary vector passing about an arbitrary point.

--- src/CustomTypes.jl
+++ src/CustomTypes.jl
@@ -26,9 +26,45 @@ macro string_as_varname_macro(s::AbstractString, v::Any)
 end
 
 function intAngle(numSides::Integer)
-    return (numSides-2) * π / numSides
+    return (numSides - 2) * π / numSides
 end
 
 function extAngle(numSides::Integer)
     return π - intAngle(numSides)
+end
+
+function arbRot3D(
+    abc::Vector{<:Real},
+    uvw::Vector{Real},
+    vec::Vector{<:Real},
+    θ::Real,
+)
+    #https://sites.google.com/site/glennmurray/Home/rotation-matrices-and-formulas/rotation-about-an-arbitrary-axis-in-3-dimensions
+    local costheta = cos(θ)
+    local onemcostheta = 1 - costheta
+    local sintheta = sin(θ)
+    local dotxyzuvw = dot(xyz, uvw)
+
+    return [
+        (abc[1] * (uvw[2]^2 + uvw[3]^2) -
+         uvw[1] * (abc[2] * uvw[2] + abc[3] * uvw[3] - dotxyzuvw)) *
+        onemcostheta +
+        xyz[1] * costheta +
+        sintheta * (-abc[3] * uvw[2] + abc[2] * uvw[3] - uvw[3] * xyz[2] +
+         uvw[2] * xyz[3])
+
+        (abc[2] * (uvw[1]^2 + uvw[3]^2) -
+         uvw[2] * (abc[1] * uvw[1] + abc[3] * uvw[3] - dotxyzuvw)) *
+        onemcostheta +
+        xyz[2] * costheta +
+        sintheta *
+        (abc[3] * uvw[1] - abc[1] * uvw[3] + uvw[3] * xyz[1] - uvw[1] * xyz[3])
+
+        (abc[3] * (uvw[1]^2 + uvw[2]^2) -
+         uvw[3] * (abc[1] * uvw[1] + abc[2] * uvw[2] - dotxyzuvw)) *
+        onemcostheta +
+        xyz[3] * costheta +
+        sintheta * (-abc[2] * uvw[1] + abc[1] * uvw[2] - uvw[2] * xyz[1] +
+         uvw[1] * xyz[2])
+    ]
 end
