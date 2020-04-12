e4cecda6d49601d56ba4daa31f75c1aa758633df
Daniel Celis Garza <celisdanieljr@gmail.com>
2020-04-12 03:45:26 +0100

    Generate documentation.

--- docs/make.jl
+++ docs/make.jl
@@ -13,9 +13,9 @@ makedocs(;
     repo="https://github.com/dcelisgarza/DDD.jl/blob/{commit}{path}#L{line}",
     sitename="DDD.jl",
     authors="Daniel Celis Garza",
-    assets=String[],
+    # assets=String[],
 )
 
 deploydocs(;
-    repo="github.com/dcelisgarza/DDD.jl",
+    repo="github.com/dcelisgarza/DDD.jl.git",
 )
