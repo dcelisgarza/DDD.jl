a79183776dc08481b3b63b99e1c82b1b96c94de6
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2021-01-28 00:59:31 +0000

    Cleanup.

--- src/Type/MaterialConstructor.jl
+++ src/Type/MaterialConstructor.jl
@@ -1,51 +1,5 @@
 """
 ```
-MaterialParameters(
-    μ::T1,
-    μMag::T1,
-    ν::T1,
-    E::T1,
-    crystalStruct::T2,
-    σPN::T1 = 0,
-) where {T1,T2 <: AbstractCrystalStruct}
-```
-Constructor for [`MaterialParameters`](@ref) automatically calculates derived quantities and sets the Peierls-Nabarro stress to a default of `σPN = 0`.
-"""
-function MaterialParameters(
-    crystalStruct::T1,
-    μ::T2,
-    μMag::T2,
-    ν::T2,
-    E::T2,
-    σPN::T2 = 0,
-) where {T1 <: AbstractCrystalStruct,T2}
-    omνInv = 1 / (1 - ν)
-    opνInv = 1 / (1 + ν)
-    νomνInv = ν * omνInv
-    νopνInv = ν * opνInv
-    μ4π = μ / (4π)
-    μ8π = μ4π / 2
-    μ4πν = μ4π * omνInv
-    νμ4πν = ν * μ4πν
-    return MaterialParameters(
-        crystalStruct,
-        μ,
-        μMag,
-        ν,
-        E,
-        omνInv,
-        opνInv,
-        νomνInv,
-        νopνInv,
-        μ4π,
-        μ8π,
-        μ4πν,
-        νμ4πν,
-        σPN,
-    )
-end
-"""
-```
 MaterialParameters(;
     μ::T1,
     μMag::T1,
@@ -72,7 +26,7 @@ function MaterialParameters(;
     μ8π = μ4π / 2
     μ4πν = μ4π * omνInv
     νμ4πν = ν * μ4πν
-    
+
     return MaterialParameters(
         crystalStruct,
         μ,
