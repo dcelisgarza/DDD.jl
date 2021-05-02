1a7bdb665db1eb3c2cd5aa5bbe1dada747995477
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2021-05-02 11:19:03 +0100

    Refactoring reducing lines.

--- src/PostProcessing/Plotting.jl
+++ src/PostProcessing/Plotting.jl
@@ -5,24 +5,8 @@ plotNodes(network::DislocationNetwork, args...; kw...)
 Plots [`DislocationNetwork`](@ref).
 """
 function plotNodes(network::DislocationNetwork, args...; kw...)
-    label = network.label
-    coord = network.coord
-    links = network.links
-    numNode = network.numNode[1]
-    elemT = eltype(links)
     fig = plot()
-    for i in 1:numNode
-        n1n2 = SVector{2, elemT}(links[1, i], links[2, i])
-        label[n1n2[1]] == extDln || label[n1n2[2]] == extDln && continue
-        plot!(fig, coord[1, n1n2], coord[2, n1n2], coord[3, n1n2], args...; kw...)
-        # quiver needs to be implemented in Plots.jl but we can use python.
-        #= 
-        lVec = coord[n2, :] - coord[n1, :]
-        quiver!([coord[n1,1]], [coord[n1,2]], [coord[n1,3]], args...; quiver=([lVec[1]], [lVec[2]], [lVec[3]]), kw...) =#
-    end
-    plot!(fig; xlabel = "x", ylabel = "y", zlabel = "z")
-
-    return fig
+    plotNodes!(fig, network, args...; kw...)
 end
 """
 ```
@@ -36,10 +20,10 @@ function plotNodes!(fig, network::DislocationNetwork, args...; kw...)
     links = network.links
     numNode = network.numNode[1]
     elemT = eltype(links)
-    for i in 1:numNode
+    @inbounds for i in 1:numNode
         n1n2 = SVector{2, elemT}(links[1, i], links[2, i])
         label[n1n2[1]] == extDln || label[n1n2[2]] == extDln && continue
-        plot!(coord[1, n1n2], coord[2, n1n2], coord[3, n1n2], args...; kw...)
+        plot!(fig, coord[1, n1n2], coord[2, n1n2], coord[3, n1n2], args...; kw...)
         #= 
         # quiver needs to be implemented in Plots.jl but we can use python.
         lVec = coord[n2, :] - coord[n1, :]
@@ -56,55 +40,8 @@ plotNodes(mesh::AbstractMesh, network::DislocationNetwork, args...; kw...)
 Plots [`DislocationNetwork`](@ref) inside [`AbstractMesh`](@ref).
 """
 function plotNodes(mesh::AbstractMesh, network::DislocationNetwork, args...; kw...)
-    label = network.label
-    coord = network.coord
-    links = network.links
-    numNode = network.numNode[1]
-    elemT = eltype(links)
     fig = plot()
-    for i in 1:numNode
-        n1n2 = SVector{2, elemT}(links[1, i], links[2, i])
-        label[n1n2[1]] == extDln || label[n1n2[2]] == extDln ? continue : nothing
-        plot!(fig, coord[1, n1n2], coord[2, n1n2], coord[3, n1n2], args...; kw...)
-        # quiver needs to be implemented in Plots.jl but we can use python.
-        #= 
-        lVec = coord[n2, :] - coord[n1, :]
-        quiver!([coord[n1,1]], [coord[n1,2]], [coord[n1,3]], args...; quiver=([lVec[1]], [lVec[2]], [lVec[3]]), kw...) =#
-    end
-
-    # Plot FEM domain.
-    vertices = reshape(collect(Iterators.flatten(mesh.vertices.vertices)), 3, :)
-    vertices = reshape(collect(Iterators.flatten(mesh.vertices.vertices)), 3, :)
-    face1 = [mesh.faces[:, 5]; mesh.faces[1, 5]]
-    face2 = [mesh.faces[:, 6]; mesh.faces[1, 6]]
-    surf1 = vertices[:, face1]
-    surf2 = vertices[:, face2]
-    plot!(fig, surf1[1, :], surf1[2, :], surf1[3, :], linecolor = :black, linewidth = 2)
-    plot!(fig, surf2[1, :], surf2[2, :], surf2[3, :], linecolor = :black, linewidth = 2)
-    side = vertices[:, [1, 5]]
-    plot!(fig, side[1, :], side[2, :], side[3, :], linecolor = :black, linewidth = 2)
-    side = vertices[:, [2, 6]]
-    plot!(fig, side[1, :], side[2, :], side[3, :], linecolor = :black, linewidth = 2)
-    side = vertices[:, [3, 7]]
-    plot!(fig, side[1, :], side[2, :], side[3, :], linecolor = :black, linewidth = 2)
-    side = vertices[:, [4, 8]]
-    plot!(fig, side[1, :], side[2, :], side[3, :], linecolor = :black, linewidth = 2)
-
-    xlims = (
-        minimum(vertices[1, :]) - 0.2 * maximum(vertices[1, :]),
-        maximum(vertices[1, :]) + 0.2 * maximum(vertices[1, :]),
-    )
-    ylims = (
-        minimum(vertices[2, :]) - 0.2 * maximum(vertices[2, :]),
-        maximum(vertices[2, :]) + 0.2 * maximum(vertices[2, :]),
-    )
-    zlims = (
-        minimum(vertices[3, :]) - 0.2 * maximum(vertices[3, :]),
-        maximum(vertices[3, :]) + 0.2 * maximum(vertices[1, :]),
-    )
-    plot!(fig, xlims = xlims, ylims = ylims, zlims = zlims)
-    plot!(fig; xlabel = "x", ylabel = "y", zlabel = "z")
-    return fig
+    plotNodes!(fig, mesh, network, args...; kw...)
 end
 
 """
@@ -125,7 +62,7 @@ function plotNodes!(
     links = network.links
     numNode = network.numNode[1]
     elemT = eltype(links)
-    for i in 1:numNode
+    @inbounds for i in 1:numNode
         n1n2 = SVector{2, elemT}(links[1, i], links[2, i])
         label[n1n2[1]] == extDln || label[n1n2[2]] == extDln ? continue : nothing
         plot!(fig, coord[1, n1n2], coord[2, n1n2], coord[3, n1n2], args...; kw...)
@@ -178,20 +115,8 @@ plotNodes(loop::DislocationLoop, args...; kw...)
 Plots [`DislocationLoop`](@ref).
 """
 function plotNodes(loop::DislocationLoop, args...; kw...)
-    idx = findall(x -> x != 0, loop.label)
-    coord = loop.coord
-    links = loop.links
-    elemT = eltype(links)
     fig = plot()
-    for i in idx
-        n1n2 = SVector{2, elemT}(links[1, i], links[2, i])
-        plot!(fig, coord[1, n1n2], coord[2, n1n2], coord[3, n1n2], args...; kw...)
-        #= 
-        # quiver needs to be implemented in Plots.jl but we can use python.
-        lVec = coord[n2, :] - coord[n1, :]
-        quiver!([coord[n1,1]], [coord[n1,2]], [coord[n1,3]], args...; quiver=([lVec[1]], [lVec[2]], [lVec[3]]), kw...) =#
-    end
-    return fig
+    plotNodes!(fig, loop::DislocationLoop, args...; kw...)
 end
 """
 ```
@@ -204,7 +129,7 @@ function plotNodes!(fig, loop::DislocationLoop, args...; kw...)
     coord = loop.coord
     links = loop.links
     elemT = eltype(links)
-    for i in idx
+    @inbounds for i in idx
         n1n2 = SVector{2, elemT}(links[1, i], links[2, i])
         plot!(fig, coord[1, n1n2], coord[2, n1n2], coord[3, n1n2], args...; kw...)
         #= 
